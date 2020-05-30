43aa556caf33b1f3985a2e0ef66254585dca4031
siwel20 <plthrock@gmail.com>
2020-05-29 20:12:58 -0600

    added drop and drag capabilities

--- assets/js/script.js
+++ assets/js/script.js
@@ -182,6 +182,72 @@ $("#remove-tasks").on("click", function() {
   saveTasks();
 });
 
+// adding sortable function
+$(".card .list-group").sortable({
+  connectWith: $(".card .list-group"),
+  scroll: false,
+  tolerance: "pointer",
+  helper: "clone",
+  activate: function(event) {
+    
+  },
+  deactivate: function(event) {
+   
+  }, 
+  over: function(event) {
+ 
+  },
+  out: function(event) {
+ 
+  },
+  update: function(event) {
+    // array to store the task data in
+    var tempArr = [];
+
+    // loop over current set of children in sortable list
+    $(this).children().each(function() {
+      var text = $(this)
+      .find("p")
+      .text()
+      .trim();
+
+      var date = $(this)
+      .find("span")
+      .text()
+      .trim();
+
+      // add task data to the temp array as an object
+      tempArr.push({
+        text: text,
+        date: date
+      });
+    });
+
+    // trim down list's ID to match object property
+    var arrName = $(this)
+    .attr("id")
+    .replace("list-", "");
+
+    // update array on tasks object and save
+    tasks[arrName] = tempArr;
+    saveTasks();
+  }
+});
+
+$("#trash").droppable({
+  accept: ".card .list-group-item",
+  tolerance: "touch",
+  drop: function(event, ui) {
+    ui.draggable.remove();
+  },
+  over: function(event, ui) {
+    
+  },
+  out: function(event, ui) {
+    
+  }
+});
+
 // load tasks for the first time
 loadTasks();
 
