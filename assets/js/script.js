4f60e909e5b51091addb84a4b9a737f84afebd43
siwel20 <plthrock@gmail.com>
2020-05-30 13:09:22 -0600

    UX enhancements

--- assets/js/script.js
+++ assets/js/script.js
@@ -63,7 +63,7 @@ $("#task-form-modal").on("shown.bs.modal", function() {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function() {
+$("#task-form-modal .btn-save").click(function() {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -204,16 +204,18 @@ $(".card .list-group").sortable({
   tolerance: "pointer",
   helper: "clone",
   activate: function(event) {
-    
+    $(this).addClass("dropover");
+    $(".bottom-trash").addClass("bottom-trash-drag");
   },
   deactivate: function(event) {
-   
+    $(this).removeClass("dropover");
+    $(".bottom-trash").removeClass("bottom-trash-drag");
   }, 
   over: function(event) {
- 
+    $(event.target).addClass("dropover-active");
   },
   out: function(event) {
- 
+    $(event.target).removeClass("dropover-active");
   },
   update: function(event) {
     // array to store the task data in
@@ -256,10 +258,10 @@ $("#trash").droppable({
     ui.draggable.remove();
   },
   over: function(event, ui) {
-    
+    $(".bottom-trash").addClass("bottom-trash-active");
   },
   out: function(event, ui) {
-    
+    $(".bottom-trash").removeClass("bottom-trash-active");
   }
 });
 
@@ -285,6 +287,12 @@ var auditTasks = function(taskEl) {
     $(taskEl).addClass("list-group-item-warning");
   }
 };
+
+setInterval(function () {
+  $(".card .list-group-item").each(function (el) {
+    auditTasks(el);
+  });
+}, 1800000);
 
 // load tasks for the first time
 loadTasks();
